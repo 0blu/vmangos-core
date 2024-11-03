47409b7b2c08477e7c4897c5d9d603cccf720f95
0blu <dot0blu@gmail.com>
2024-11-04 00:49:16 +0100

    GetCurrentProcessAmountOfAllocatedBytes linux support

--- src/shared/IO/Utils.cpp
+++ src/shared/IO/Utils.cpp
@@ -3,8 +3,9 @@
 #if defined(WIN32)
 #include <Windows.h>
 #include <psapi.h>
-#else
+#elif defined(__linux__)
 #include <unistd.h>
+#include <sys/resource.h>
 #endif
 
 uint64_t IO::Utils::GetCurrentProcessId()
@@ -18,11 +19,19 @@ uint64_t IO::Utils::GetCurrentProcessId()
 
 uint64_t IO::Utils::GetCurrentProcessAmountOfAllocatedBytes()
 {
-#ifdef WIN32
+#if defined(WIN32)
     PROCESS_MEMORY_COUNTERS_EX pmc;
     ::GetProcessMemoryInfo(GetCurrentProcess(), reinterpret_cast<PROCESS_MEMORY_COUNTERS*>(&pmc), sizeof(pmc));
     return pmc.PrivateUsage;
+#elif defined(__linux__)
+    rusage usage;
+    if (::getrusage(RUSAGE_SELF, &usage) != 0)
+        return 0;
+
+    return usage.ru_maxrss * 1024; // Convert from kilobytes to bytes
+#elif defined(__APPLE__)
+    #error "TODO"
 #else
-    #error TODO cache static: `fd = open("/proc/self/statm", O_RDONLY);`
+#error "IO::Utils::GetCurrentProcessAmountOfAllocatedBytes() not supported on your platform"
 #endif
 }
