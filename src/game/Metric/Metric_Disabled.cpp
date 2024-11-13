3fdb854b24b5ef301f49ecbcc2c718ed540bac5f
0blu <dot0blu@gmail.com>
2024-11-13 02:00:30 +0100

    Metric fix windows build

--- src/game/Metric/Metric_Disabled.cpp
+++ src/game/Metric/Metric_Disabled.cpp
@@ -5,7 +5,7 @@ void MaNGOS::Metric::MetricService::Finalize()
 {
 }
 
-bool MaNGOS::Metric::MetricService::Initialize(GraphiteDbClientConfig const& config, std::string const& metricPrefix, std::chrono::seconds sendingInterval)
+bool MaNGOS::Metric::MetricService::Initialize(GraphiteDbClientConfig const& config, std::string const& metricPrefix, IO::IoContext* ioContext, std::chrono::seconds sendingInterval)
 {
     sLog.Out(LOG_BASIC, LOG_LVL_ERROR, "Metrics are disabled in this build. Please build with -DENABLE_METRICS=On to enable them");
     return false;
