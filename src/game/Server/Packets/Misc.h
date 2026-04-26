ab6229f745cd2b7fb06e6f849417c9616f520d17
0blu <dot0blu@gmail.com>
2026-04-26 23:38:33 +0200

    Refactor SMSG_ sends in Social/Warden/BG/Group/LFG to ServerPacket classes (#30)

--- src/game/Server/Packets/Misc.h
+++ src/game/Server/Packets/Misc.h
@@ -360,17 +360,37 @@ namespace WorldPackets { namespace Misc
     };
 
 #if SUPPORTED_CLIENT_BUILD > CLIENT_BUILD_1_5_1
-    class WardenData final : public ClientPacket
-    {
-    public:
-        std::vector<uint8> data;
-
-        explicit WardenData() : ClientPacket(CMSG_WARDEN_DATA) {}
-        void ReadFromWorldPacket(WorldPacket& recv_data) override;
-    };
+    // NOTE: CMSG_WARDEN_DATA/SMSG_WARDEN_DATA packet structs live in `Packets/Warden.h`
+    //       (`WorldPackets::Warden` namespace).
 #endif
 
     // --- Server Packets ---
+
+    // SMSG_MEETINGSTONE_MEMBER_ADDED: notifies group members that a new player was added via LFG
+    class MeetingstoneMemberAdded final : public ServerPacket
+    {
+    public:
+        ObjectGuid playerGuid; // guid of the player that was added
+
+        explicit MeetingstoneMemberAdded() : ServerPacket(SMSG_MEETINGSTONE_MEMBER_ADDED) {}
+        void AppendBodyTo(ByteBuffer& buffer) const override;
+    };
+
+    // SMSG_MEETINGSTONE_IN_PROGRESS: empty body; sent periodically while the LFG queue is still searching
+    class MeetingstoneInProgress final : public ServerPacket
+    {
+    public:
+        explicit MeetingstoneInProgress() : ServerPacket(SMSG_MEETINGSTONE_IN_PROGRESS) {}
+        void AppendBodyTo(ByteBuffer& buffer) const override;
+    };
+
+    // SMSG_MEETINGSTONE_COMPLETE: empty body; sent when the LFG search completes successfully
+    class MeetingstoneComplete final : public ServerPacket
+    {
+    public:
+        explicit MeetingstoneComplete() : ServerPacket(SMSG_MEETINGSTONE_COMPLETE) {}
+        void AppendBodyTo(ByteBuffer& buffer) const override;
+    };
 
     class LogoutComplete final : public ServerPacket
     {
