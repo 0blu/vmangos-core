cbf6187b291d8acd67611fca779cf65f0ba45a86
0blu <dot0blu@gmail.com>
2026-06-03 21:09:19 +0200

    Update AuctionHouse.cpp

--- src/game/Server/Packets/AuctionHouse.cpp
+++ src/game/Server/Packets/AuctionHouse.cpp
@@ -1,5 +1,6 @@
 #include "AuctionHouse.h"
 // Needed for AuctionAction/AuctionError enum constants used by AuctionCommandResult serialization.
+#include "AuctionHouseMgr.h"
 #include "SharedDefines.h"
 
 void WorldPackets::AuctionHouse::AuctionHello::ReadFromWorldPacket(WorldPacket& recv_data)
